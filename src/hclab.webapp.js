4c9ed60184251cfefb3e54f023c08434b58840c4
A. O. Hycent <101049169+aohycent@users.noreply.github.com>
2025-06-16 23:54:42 +0000

    Update hclab.webapp.js

--- src/hclab.webapp.js
+++ src/hclab.webapp.js
@@ -42,4 +42,18 @@
     }
     return false;
   }
+  function byteLength(str) {
+    if (window.Blob) {
+      try { return new Blob([str]).size; } catch (e) {}
+    }
+    var s = str.length;
+    for (var i=str.length-1; i>=0; i--) {
+      var code = str.charCodeAt(i);
+      if (code > 0x7f && code <= 0x7ff) s++;
+      else if (code > 0x7ff && code <= 0xffff) s+=2;
+      if (code >= 0xdc00 && code <= 0xdfff) i--;
+    }
+    return s;
+  }
+  
 })();
